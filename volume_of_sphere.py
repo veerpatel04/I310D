52de9a28a16de6bec976e5d960fb1e9e53cc81a8
Veer Patel <veerpatel2004@gmail.com>
2023-09-21 16:17:56 -0500

    volume_of_sphere.py added

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+def compute_volume_of_sphere(radius):
+	pi = 3.14
+	area = pi * radius * radius * radius * (4/3)
+	return volume
+
+radius1 = 30
+vol1 = compute_volume_of_circle(radius1)
+print(f"The volume of a sphere with radius {radius1} is: {vol1}")
+
+radius2 = 40
+vol2 = compute_volume_of_circle(radius2)
+print(f"The volume of a sphere with radius {radius2} is: {vol2}")
